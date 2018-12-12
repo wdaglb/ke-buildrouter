f129fd201b672ee5eb58b6f8edddc92fb977d0ae
公司 <admin@cysm.com>
2018-12-12 12:55:22 +0800

    init

--- bootstrap.php
+++ bootstrap.php
@@ -5,12 +5,15 @@
 // | Author: King east <1207877378@qq.com>
 // +----------------------------------------------------------------------
 
-if (\think\facade\App::isDebug()) {
-    \think\facade\Hook::exec(function () {
-        $builder = new \ke\BuildRoute(\think\facade\App::getRootPath(), 'application', \think\facade\App::getRootPath() . 'route/build_route.php');
-        $builder->make();
-    });
+// 判断TP是否启动
+if (class_exists('\\think\\facade\\App')) {
+    if (\think\facade\App::isDebug()) {
+        \think\facade\Hook::exec(function () {
+            $builder = new \ke\BuildRoute(\think\facade\App::getRootPath(), 'application', \think\facade\App::getRootPath() . 'route/build_route.php');
+            $builder->make();
+        });
+    }
+    \think\Console::addDefaultCommands([
+        '\\ke\\Command'
+    ]);
 }
-\think\Console::addDefaultCommands([
-    '\\ke\\Command'
-]);

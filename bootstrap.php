e2333bd71c3bc741779a65b5171576f42610baf9
公司 <admin@cysm.com>
2018-12-12 12:54:35 +0800

    init

--- bootstrap.php
+++ bootstrap.php
@@ -5,6 +5,12 @@
 // | Author: King east <1207877378@qq.com>
 // +----------------------------------------------------------------------
 
+if (\think\facade\App::isDebug()) {
+    \think\facade\Hook::exec(function () {
+        $builder = new \ke\BuildRoute(\think\facade\App::getRootPath(), 'application', \think\facade\App::getRootPath() . 'route/build_route.php');
+        $builder->make();
+    });
+}
 \think\Console::addDefaultCommands([
     '\\ke\\Command'
 ]);

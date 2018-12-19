c4b0e01305d160fc9b131fbe3b7608634f4e65a0
公司 <admin@cysm.com>
2018-12-19 16:22:48 +0800

    version

--- bootstrap.php
+++ bootstrap.php
@@ -5,6 +5,14 @@
 // | Author: King east <1207877378@qq.com>
 // +----------------------------------------------------------------------
 
+if (defined('THINK_VERSION')) {
+    define('THINK_VERSION', '5.0');
+} else {
+    define('THINK_VERSION', '5.1');
+}
+
+var_dump(THINK_VERSION);exit;
+
 if (\think\facade\App::isDebug()) {
     \think\facade\Hook::exec(function () {
         $builder = new \ke\BuildRouter(\think\facade\App::getRootPath(), 'application', \think\facade\App::getRootPath() . 'route/build_route.php');
